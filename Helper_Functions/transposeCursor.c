292077196bd1dc769c1e76498d26668a5be03f50
watermelon2718 <ellyntran@u.boisestate.edu>
2026-04-16 14:54:38 -0600

    Create transposeCursor.c

---
+++ Helper_Functions/transposeCursor.c
@@ -0,0 +1,12 @@
+// Interrupt handler for vertical to horizontal switch -- on button press
+/* 
+Handlers
+interrupt handler for vertical to horizontal switch – on button press
+vert to horiz
+y pos stays
+x pos goes to (x)/2 if odd, and (x-1)/2 if even
+horiz to vert
+y pos = y unless 2, = 1
+x pos = x*2 - 1
+*/
+
